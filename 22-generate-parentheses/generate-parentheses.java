f0529ac6110e0a1e129952a7b729f36119005f73
Saurabh090805 <154251728+Saurabh090805@users.noreply.github.com>
2026-09-01 22:47:58 +0530

    Time: 2 ms (69.54%) | Memory: 44.4 MB (72.87%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    public List<String> generateParenthesis(int n) {
+
+        List<String> result = new ArrayList<>();
+
+        backtrack(result, "", 0, 0, n);
+
+        return result;
+    }
+
+    private void backtrack(
+        List<String> result,
+        String current,
+        int open,
+        int close,
+        int n
+    ) {
+
+        // We have used all parentheses
+        if (open == n && close == n) {
+            result.add(current);
+            return;
+        }
+
+        // Add '(' if we still have some left
+        if (open < n) {
+            backtrack(
+                result,
+                current + "(",
+                open + 1,
+                close,
+                n
+            );
+        }
+
+        // Add ')' only when there is an unmatched '('
+        if (close < open) {
+            backtrack(
+                result,
+                current + ")",
+                open,
+                close + 1,
+                n
+            );
+        }
+    }
+}
